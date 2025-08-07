ed56d98017c3110c426bbcdc9335c2a2c390eb2d
pingu01 <gabrielmnascimento04@gmail.com>
2025-08-07 00:45:48 -0300

    Remove Home Manager configuration from flake.nix and flake.lock

--- flake.nix
+++ flake.nix
@@ -5,60 +5,35 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
-  {
-    darwinConfigurations."mac" = nix-darwin.lib.darwinSystem {
-      modules = [
-        # Inline system configuration - no external modules
-        {
-          nix.settings.experimental-features = "nix-command flakes";
-          system.stateVersion = 5;
-          nixpkgs.hostPlatform = "aarch64-darwin";
-          nixpkgs.config.allowUnfree = true;
-          
-          environment.systemPackages = with nixpkgs.legacyPackages.aarch64-darwin; [
-            git
-            vim
-            curl
-          ];
-        }
+  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  let
+    configuration = { pkgs, ... }: {
+      # Enable Nix flakes
+      nix.settings.experimental-features = "nix-command flakes";
+      
+      # Set system state version
+      system.stateVersion = 5;
+      
+      # Basic packages
+      environment.systemPackages = with pkgs; [
+        git
+        vim
+        curl
+        wget
+        neovim
         
-        # Home Manager with inline configuration
-        home-manager.darwinModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.pingu = { pkgs, ... }: {
-            # Required settings
-            home.username = "pingu";
-            home.homeDirectory = "/Users/pingu";
-            home.stateVersion = "23.11";
-            
-            # Packages
-            home.packages = with pkgs; [
-              nodejs
-              python3
-            ];
-            
-            # Programs
-            programs.git = {
-              enable = true;
-              userName = "Pingu";
-              userEmail = "pingu@example.com";
-            };
-            
-            programs.zsh = {
-              enable = true;
-              shellAliases.ll = "ls -la";
-            };
-          };
-        }
       ];
+      
+      # Set platform (change to x86_64-darwin for Intel Macs)
+      nixpkgs.hostPlatform = "aarch64-darwin";
+    };
+  in
+  {
+    # Replace "YourMacName" with your actual hostname or any name you prefer
+    darwinConfigurations."mac" = nix-darwin.lib.darwinSystem {
+      modules = [ configuration ];
     };
   };
 }
